e5ab70fa662e6d9dc93a8cfa59ab60ca22c2726c
Oliver Ni <oliverni@berkeley.edu>
2024-09-27 15:13:45 -0700

    wip: greetd

--- flake.nix
+++ flake.nix
@@ -21,9 +21,15 @@
       url = "github:ocf/utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    ocf-greeter = {
+      url = "git+file:///Users/oliver/Development/github.com/ocf/greeter";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.systems.follows = "systems";
+    };
     wayout = {
       url = "github:ocf/wayout";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.systems.follows = "systems";
     };
   };
 
@@ -37,6 +43,7 @@
     , ocf-sync-etc
     , ocf-pam-trimspaces
     , ocf-utils
+    , ocf-greeter
     , wayout
     }@inputs:
     let
@@ -50,6 +57,7 @@
         ocf-sync-etc.overlays.default
         ocf-pam-trimspaces.overlays.default
         nix-index-database.overlays.nix-index
+        ocf-greeter.overlays.default
 
         # GNOME 46: triple-buffering-v4-46
         # See: https://nixos.wiki/wiki/GNOME#Dynamic_triple_buffering
--- modules/ocf/graphical.nix
+++ modules/ocf/graphical.nix
@@ -15,6 +15,12 @@ let
       openssh = pkgs.openssh_gssapi;
     };
   };
+
+  swayConfig = pkgs.writeText "sway-config" ''
+    output * scale 2
+    default_border none
+    exec "systemd-cat -t ocf-greeter ${lib.getExe pkgs.ocf-greeter} --default-session plasma; swaymsg exit"
+  '';
 in
 {
   options.ocf.graphical = {
@@ -24,12 +30,13 @@ in
   config = lib.mkIf cfg.enable {
     security.pam = {
       # Mount ~/remote
-      services.login.pamMount = true;
-      services.login.rules.session.mount.order = config.security.pam.services.login.rules.session.krb5.order + 50;
+      services.greetd.pamMount = true;
+      services.greetd.makeHomeDir = true;
+      services.greetd.rules.session.mount.order = config.security.pam.services.login.rules.session.krb5.order + 50;
       mount.extraVolumes = [ ''<volume fstype="fuse" path="${lib.getExe sshfs}#%(USER)@tsunami:" mountpoint="~/remote/" options="follow_symlinks,UserKnownHostsFile=/dev/null,StrictHostKeyChecking=no" pgrp="ocf" />'' ];
 
-      # Trim spaces from username
-      services.login.rules.auth.trimspaces = {
+      # # Trim spaces from username
+      services.greetd.rules.auth.trimspaces = {
         control = "requisite";
         modulePath = "${pkgs.ocf-pam_trimspaces}/lib/security/pam_trimspaces.so";
         order = 0;
@@ -76,34 +83,30 @@ in
 
     fonts.packages = with pkgs; [ meslo-lgs-nf noto-fonts noto-fonts-cjk-sans noto-fonts-extra ];
 
+    programs.sway.enable = true;
+
     services = {
-      # KDE Plasma is our primary DE, but have others available
+      displayManager.defaultSession = "plasma";
+
       desktopManager.plasma6.enable = true;
-      xserver.desktopManager = {
-        gnome.enable = true;
-        xfce.enable = true;
-      };
 
-      displayManager = {
-        defaultSession = "plasma";
-
-        sddm = {
-          enable = true;
-          theme = "catppuccin-latte";
-          wayland.enable = true;
-          settings.Users = {
-            RememberLastUser = false;
-            RememberLastSession = false;
-          };
+      xserver = {
+        enable = true;
+        desktopManager = {
+          # KDE Plasma is our primary DE, but have others available
+          gnome.enable = true;
+          xfce.enable = true;
         };
       };
     };
 
-    # KDE 6.0.3 has a bug that breaks logging out within the first 60 seconds.
-    # This is caused by the DrKonqi service's ExecStartPre command, which sleeps
-    # for 60 seconds to let the system settle before monitoring coredumps. We
-    # don't need this wait, so we remove the ExecStartPre entry.
-    systemd.user.services.drkonqi-coredump-pickup.unitConfig.ExecStartPre = lib.mkForce [ ];
+    services.greetd = {
+      enable = true;
+      settings.default_session = {
+        command = "${lib.getExe pkgs.sway} --config ${swayConfig} --unsupported-gpu";
+        # command = "${lib.getExe pkgs.cage} -s -- systemd-cat -t ocf-greeter ${lib.getExe pkgs.ocf-greeter}";
+      };
+    };
 
     systemd.user.services.wayout = {
       description = "Automatic idle logout manager";
